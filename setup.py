ccec4dcd1565a97514fc607c234d2a75cf7ee773
Andrea Wright <71980564+awright813@users.noreply.github.com>
2024-10-27 21:19:49 -0700

    bug fix

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="ghosted",
-    version="1.0.1",
+    version="1.0.2",
     description="A Python library for synthetic data generation, data blending, anomaly injection, and noise injection.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Andrea Wright Morgan",
     author_email="andreawright0813@gmail.com",
     url="https://github.com/awright813/ghosted",
-    packages=find_packages(),
+    packages=find_packages(include=["ghosted", "ghosted.*"]),
     include_package_data=True,
     install_requires=[
         "copulas>=0.11.1",
